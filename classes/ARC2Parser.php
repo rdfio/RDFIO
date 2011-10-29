ffa88a9fb8f335dfcdd0f1b55102a4e88856bd8a
Samuel Lampa <samuel.lampa@rilnet.com>
2011-10-29 14:27:40 +0200

    Test init RDFIOARC2Parser class

---
+++ classes/ARC2Parser.php
@@ -0,0 +1,15 @@
+<?php
+
+class RDFIOARC2Parser extends RDFIOParser {
+	public $mInput = null;
+	public $mResults = null;
+	
+	function __construct() {
+		// Nothing so far
+	}
+	
+	public function execute() {
+		// Do stuff ...
+	}
+	
+}
