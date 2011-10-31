d70089d375bf9a2248c4901d72317a6f99de28ef
Samuel Lampa <samuel.lampa@rilnet.com>
2011-10-31 18:18:53 +0100

    Starting work on general Data container class. Adding ARC2 getters and setters

--- classes/Data.php
+++ classes/Data.php
@@ -3,6 +3,27 @@
 class RDFIOData {
 	protected $mData = null;
 	protected $mDataType = null;
+	protected $mARC2TripleSetToSMWParser = null;
+	protected $mSMWToARC2TripleSetParser = null;
+	protected $mARC2ResourceIndexToSMWParser = null;
+	protected $mSMWToARC2ResourceIndexParser = null;
+		
+	public function __construct() {
+		// TODO: Add code
+	}
+	
+	public function setFromARC2TripleSet( $arc2triplesData ) {
+		// TODO: Add code
+	}
+	public function getAsARC2TripleSet( $arc2triplesData ) {
+		// TODO: Add code		
+	}
+	public function setFromARC2ResourceIndex( $arc2triplesData ) {
+		// TODO: Add code		
+	}
+	public function getAsARC2ResourceIndex( $arc2triplesData ) {
+		// TODO: Add code		
+	}
 	
 	# Getters and setters
 	
