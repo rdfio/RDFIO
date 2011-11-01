a2d50440915b6c14d2995303d188d5a86e6add1c
Samuel Lampa <samuel.lampa@rilnet.com>
2011-11-01 15:42:05 +0100

    Starting to code the ARC2ToRDFIO Parser. Realizing some implications on data structure ...

--- classes/parsers/RDFIO_ARC2ToRDFIOParser.php
+++ classes/parsers/RDFIO_ARC2ToRDFIOParser.php
@@ -11,7 +11,57 @@ class RDFIOARC2ToRDFIOParser extends RDFIOParser {
 	public function execute() {
 		$arc2triples = $this->getInput();
 		
-		// TODO: Do stuff ...
+		/**
+		 * Data structure of one ARC2 triple:
+		 * Array
+		 * (
+		 *     [s] => http://bio2rdf.org/go:0032283
+		 *     [p] => http://bio2rdf.org/go_resource:accession
+		 *     [o] => GO:0032283
+		 *     [s_type] => uri
+		 *     [o_type] => literal
+		 *     [o_datatype] => 
+		 *     [o_lang] => 
+		 * )
+		 */
+		
+		foreach ( $arc2triples as $arc2triple ) {
+			$subjectString = $arc2triple['s'];
+			$predicateString = $arc2triple['p'];
+			$objectString = $arc2triple['o'];
+			$subjectTypeString = $arc2triple['s_type'];
+			$objectTypeString = $arc2triple['o_type'];
+			$objectDataTypeString = $arc2triple['o_datatype'];
+			$objectLangString = $arc2triple['o_lang'];
+
+			# Subject			
+			switch ( $subjectTypeString ) {
+				case 'uri':
+					$subject = RDFIOURI::newFromString( $subjectString );
+					break;
+				case 'literal':
+					$subject = RDFIOLiteral::newFromString( $subjectString );
+			}
+			
+			# Predicate
+			$predicate = RDFIOURI::newFromString( $predicateString );
+			
+			# Object
+			switch ( $objectTypeString ) {
+				case 'uri':
+					$object = RDFIOURI::newFromString( $objectString );
+					break;
+				case 'literal':
+					$object = RDFIOLiteral::newFromString( $objectString );
+			}
+			
+			# TODO: Ahh ... right ... I maybe shouldnät have a specialized URI/Literal
+			# object, since I need methods to "getAsWikiPage" for both ... and then it
+			# is easier to have just "Resource" class, with the type (uri/literal) stored
+			# as a variable ... or else I'll need to type resolution and stuff ... 
+			
+		} 
+
 	}
 	
 }
