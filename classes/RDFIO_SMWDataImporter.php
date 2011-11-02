73386535f7acdbe5db83f427b65b0593e7799c88
Samuel Lampa <samuel.lampa@rilnet.com>
2011-11-02 04:49:06 +0100

    Got WikiObjectModel example to work

--- classes/RDFIO_SMWDataImporter.php
+++ classes/RDFIO_SMWDataImporter.php
@@ -13,12 +13,37 @@ class RDFIOSMWDataImporter {
 		# TODO: Decide what the WikiWriter should do ...
 		# ... maybe single page edits?
 		
-		# TODO: Ahh .. need to decide whether to store 
-		# facts as triples or "resource indexes" ...
-		# the latter of course more closely maps to 
-		# SMW, since there is then one resource index
-		# per wiki page, and that corresponds to one
-		# SemanticData object ...
+		$title = Title::newFromText('Test2');
+		
+		$wom = WOMProcessor::getPageObject($title);
+		$sentence_obj = null;
+		try{
+			$oid = WOMProcessor::getObjIdByXPath($title, '//sentence[1]');
+			// use page object functions
+			$sentence_obj = $wom->getObject($oid[0]);
+		} catch( Exception $e ) {
+			return;
+		}
+		
+		$text_obj = null;
+		foreach( $sentence_obj->getObjects() as $sub_obj) {
+			if($sub_obj->getTypeID() == WOM_TYPE_TEXT) {
+				$text_obj = $sub_obj;
+				break;
+			}
+		}
+		
+		$text_obj->setText('Hi, world.');
+		$article = new Article($title);
+		$content = $wom->getWikiText();
+		$summary = "Edited by WOM...";
+		$article->doEdit( $content, $summary );
+		
+		// $article = new Article($title);
+		// $summary = "A Bot edit ...";
+		// $content = $article->fetchContent();
+		// $content_new = $content . ' ... some more content';
+		// $article->doEdit($content_new, $summary);
 		
 		// $this->mWikiWriter->setInput( $results );
 		// $this->mWikiWriter->execute();
