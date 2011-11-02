64a3974ef55122cc203b6806649a6a215aedf338
Samuel Lampa <samuel.lampa@rilnet.com>
2011-11-02 02:44:10 +0100

    Adding getters and setters to Fact class

--- classes/data/RDFIO_Fact.php
+++ classes/data/RDFIO_Fact.php
@@ -9,4 +9,17 @@ class RDFIOFact {
 		// ...
 	}
 	
+	# Getters and setters
+	public function getPredicate() { 
+	    return $this->mPredicate;
+	}
+	public function setPredicate( $predicate ) { 
+	    $this->mPredicate = $predicate;
+	}
+	public function getObject() { 
+	    return $this->mObject;
+	}
+	public function setObject( $object ) { 
+	    $this->mObject = $object;
+	}
 }
