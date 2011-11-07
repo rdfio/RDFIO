161edcbef1db4bf7dd7c2af9a0a71ea74975ffe3
Samuel Lampa <samuel.lampa@rilnet.com>
2011-11-07 23:28:48 +0100

    Polishing ...

--- classes/parsers/RDFIO_EquivalentURIPropertyCreator.php
+++ classes/parsers/RDFIO_EquivalentURIPropertyCreator.php
@@ -13,17 +13,17 @@ class RDFIOEquivalentURIPropertyCreator {
 			# Subject
 			$uriResolverURI = RDFIOARC2StoreWrapper::getURIResolverURI();	
 			$subjectURIStr = $uriResolverURI . $origDataURI->getAsWikiPageName();
-			$subjectURIStr = str_replace( ' ', '_', $subjectURIStr );
+			$subjectURIStr = str_replace( ' ', '_', $subjectURIStr ); // TODO: Replace with xmlify method!
 			$subjectURI = RDFIOURI::newFromString($subjectURIStr, $equivURIFactsDataAggregate);
 			
 			# Predicate
 			// TODO: Should one use the Equivalent URI resolver URI, or rather the owl#sameAs at once?
-			$equivURIPropertyURI = $uriResolverURI . 'Equivalent_URI';
-			$equivURIpropURI = RDFIOURI::newFromString($equivURIPropertyURI, $equivURIFactsDataAggregate);
+			$equivURIPropertyURIStr = $uriResolverURI . 'Equivalent_URI';
+			$equivURIpropURI = RDFIOURI::newFromString($equivURIPropertyURIStr, $equivURIFactsDataAggregate);
 			
 			# Object
-			$equivURIURIString = $origDataURI->getIdentifier();
-			$equivURIURI = RDFIOLiteral::newFromString( $equivURIURIString, $equivURIFactsDataAggregate );
+			$equivURIURIStr = $origDataURI->getIdentifier();
+			$equivURIURI = RDFIOLiteral::newFromString( $equivURIURIStr, $equivURIFactsDataAggregate );
 			
 			# Connecting things ...
 			$equivURIFact = RDFIOFact::newFromPredicateAndObject( $equivURIpropURI, $equivURIURI );
