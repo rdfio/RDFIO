2fbd813c81dda44829385a3d71a6382d076150bf
Samuel Lampa <samuel.lampa@gmail.com>
2012-10-20 09:40:18 +0200

    Started work on re-writing the converting functionality, in more straight-forward manner

---
+++ classes/parsers/RDFIO_ARC2ToWikiConverter.php
@@ -0,0 +1,15 @@
+<?php
+
+class RDFIOARC2ToWikiConverter extends RDFIOParser {
+	
+	public function __construct() {
+		// ...
+	}
+	
+	public function parseData( $arc2ResourceIndex, $arc2NameSpacePrefixes ) {
+		
+		
+	}
+	
+}
+
