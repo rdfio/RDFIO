dbbc69b8d74a38c74a16039b55f45ec4f42009fc
Samuel Lampa <samuel.lampa@gmail.com>
2012-12-01 18:19:47 +0100

    A little more interesting example data

--- specials/SpecialRDFImport_body.php
+++ specials/SpecialRDFImport_body.php
@@ -101,26 +101,34 @@ class RDFImport extends SpecialPage {
 	 */
 	public function getExampleRDFXMLData() {
 		return '<rdf:RDF\\n\
-xmlns:rdf=\\"http://www.w3.org/1999/02/22-rdf-syntax-ns#\\"\\n\
-xmlns:cd=\\"http://www.recshop.fake/cd#\\">\\n\
+    xmlns:rdf=\\"http://www.w3.org/1999/02/22-rdf-syntax-ns#\\"\\n\
+    xmlns:cd=\\"http://www.recshop.fake/cd#\\"\\n\
+    xmlns:countries=\\"http://www.countries.org/onto/\\"\\n\
+    xmlns:rdfs=\\"http://www.w3.org/2000/01/rdf-schema#\\"\\n\
+    >\\n\
 \\n\
-<rdf:Description\\n\
-rdf:about=\\"http://www.recshop.fake/cd/Empire Burlesque\\">\\n\
-  <cd:artist>Bob Dylan</cd:artist>\\n\
-  <cd:country>USA</cd:country>\\n\
-  <cd:company>Columbia</cd:company>\\n\
-  <cd:price>10.90</cd:price>\\n\
-  <cd:year>1985</cd:year>\\n\
-</rdf:Description>\\n\
+    <rdf:Description\\n\
+        rdf:about=\\"http://www.recshop.fake/cd/Empire Burlesque\\">\\n\
+          <cd:artist>Bob Dylan</cd:artist>\\n\
+            <cd:country rdf:resource=\\"http://www.countries.org/onto/USA\\"/>\\n\
+              <cd:company>Columbia</cd:company>\\n\
+                <cd:price>10.90</cd:price>\\n\
+                  <cd:year>1985</cd:year>\\n\
+              </rdf:Description>\\n\
 \\n\
-<rdf:Description\\n\
-rdf:about=\\"http://www.recshop.fake/cd/Hide your heart\\">\\n\
-  <cd:artist>Bonnie Tyler</cd:artist>\\n\
-  <cd:country>UK</cd:country>\\n\
-  <cd:company>CBS Records</cd:company>\\n\
-  <cd:price>9.90</cd:price>\\n\
-  <cd:year>1988</cd:year>\\n\
-</rdf:Description>\\n\
+    <rdf:Description\\n\
+        rdf:about=\\"http://www.recshop.fake/cd/Hide your heart\\">\\n\
+            <cd:artist>Bonnie Tyler</cd:artist>\\n\
+            <cd:country>UK</cd:country>\\n\
+            <cd:company>CBS Records</cd:company>\\n\
+            <cd:price>9.90</cd:price>\\n\
+            <cd:year>1988</cd:year>\\n\
+    </rdf:Description>\\n\
+\\n\
+    <rdf:Description\\n\
+       rdf:about=\\"http://www.countries.org/onto/USA\\">\\n\
+            <rdfs:label>USA</rdfs:label>\\n\
+    </rdf:Description>\\n\
 </rdf:RDF>';
 	}
 
