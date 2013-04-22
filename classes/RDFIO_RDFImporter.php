28ef4fb3c2a3388ab03743fdb2a6fc369ac41d82
Samuel Lampa <samuel.lampa@gmail.com>
2013-04-22 17:29:30 +0200

    Code style cleanup of RDFIO RDF Importer

--- classes/RDFIO_RDFImporter.php
+++ classes/RDFIO_RDFImporter.php
@@ -2,41 +2,35 @@
 
 class RDFIORDFImporter {
 	
-	function __construct() {
-	
-	}
+	function __construct() {}
 
 	public function importRdfXml( $importData ) {
-		# Parse RDF/XML to triples
+		// Parse RDF/XML to triples
 		$arc2rdfxmlparser = ARC2::getRDFXMLParser();
 		$arc2rdfxmlparser->parseData( $importData );
 
-		# Receive the data
+		// Receive the data
 		$triples = $arc2rdfxmlparser->triples;
-		$tripleindex = $arc2rdfxmlparser->getSimpleIndex();
+		$tripleIndex = $arc2rdfxmlparser->getSimpleIndex();
 		$namespaces = $arc2rdfxmlparser->nsp;
 		
-		$this->importFromArc2Data( $triples, $tripleindex, $namespaces );
+		$this->importFromArc2Data( $triples, $wikiPages, $namespaces );
 	}
 
 	public function importTriples( $triples ) {
 		$this->importFromArc2Data( $triples );
 	}
 
-	// TODO: Add other formats here later ...
-
 	private function importFromArc2Data( $triples, $tripleIndex="", $namespaces="" ) {
 		global $wgOut;
 		
-        # Parse data from ARC2 triples to custom data structure holding wiki pages
+        // Parse data from ARC2 triples to custom data structure holding wiki pages
         $arc2towikiconverter = new RDFIOARC2ToWikiConverter();
-        $wikipages = $arc2towikiconverter->convert( $triples, $tripleIndex, $namespaces );
+        $wikiPages = $arc2towikiconverter->convert( $triples, $tripleIndex, $namespaces );
         
-        # Import pages into wiki
+        // Import pages into wiki
         $smwPageWriter = new RDFIOSMWPageWriter();
-        $smwPageWriter->import( $wikipages );
+        $smwPageWriter->import( $wikiPages );
 	}
 
 }
-
-?>
