151a747afc14e8368cfdbdc74ca8b966bdfdb623
Samuel Lampa <samuel.lampa@gmail.com>
2013-10-09 15:03:49 +0200

    Added stubs for some tests

---
+++ tests/phpunit/RDFIOTest.php
@@ -0,0 +1,16 @@
+<?php
+
+class RDFIOTest extends PHPUnit_Framework_TestCase {
+
+	function setUp() {}
+	function tearDown() {}
+	
+	/**
+	 * Simple test to see that the PHPUnit test framework 
+	 * (And the MakeGood Eclipse plugin) is correctly set up. 
+	 */
+	public function testRdfio() {
+		$this->assertTrue(true);
+	}
+	
+}
---
+++ tests/phpunit/specials/SpecialRDFImport_bodyTest.php
@@ -0,0 +1,16 @@
+<?php
+
+class RDFImportTest extends PHPUnit_Framework_TestCase {
+	
+	function setUp() {}
+	function tearDown() {}
+
+	/**
+	 * Simple test to see that the PHPUnit test framework
+	 * (And the MakeGood Eclipse plugin) is correctly set up.
+	 */
+	public function testPhpUnitSetup() {
+		$this->assertTrue(true);
+	}
+	
+}
