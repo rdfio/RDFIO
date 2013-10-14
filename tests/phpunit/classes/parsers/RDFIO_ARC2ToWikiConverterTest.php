989f0c90d04de79708346e06091a7b708d6d8003
Samuel Lampa <samuel.lampa@gmail.com>
2013-10-14 14:48:53 +0200

    Moved test data out to separate class

--- tests/phpunit/classes/parsers/RDFIO_ARC2ToWikiConverterTest.php
+++ tests/phpunit/classes/parsers/RDFIO_ARC2ToWikiConverterTest.php
@@ -12,10 +12,10 @@ class RDFIOARC2ToWikiConverterTest extends MediaWikiTestCase {
 	 * @covers RDFIOARC2ToWikiConverter
 	 */
 	public function testConvert() {
-		$arc2rdfxmlparser = ARC2::getRDFXMLParser();
-		$importData = $this->getTestImportData();
-	
-		$arc2rdfxmlparser->parseData( $importData );
+	    $testData = new RDFIOTestData();
+	    
+	    $arc2rdfxmlparser = ARC2::getRDFXMLParser();
+		$arc2rdfxmlparser->parseData( $testData->getTestImportData() );
 	
 		$triples = $arc2rdfxmlparser->triples;
 		$tripleIndex = $arc2rdfxmlparser->getSimpleIndex();
@@ -26,45 +26,6 @@ class RDFIOARC2ToWikiConverterTest extends MediaWikiTestCase {
 	
 		$this->assertEquals(11, count($wikiPages), "No wiki pages converted from triples!");
 	}	
-	
-	
-	function getTestImportData() {
-		$testImportData = '<rdf:RDF
-				xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
-				xmlns:cd="http://www.recshop.fake/cd#"
-				xmlns:countries="http://www.countries.org/onto/"
-				xmlns:rdfs="http://www.w3.org/2000/01/rdf-schema#"
-				>
-	
-				<rdf:Description
-				rdf:about="http://www.recshop.fake/cd/Empire Burlesque">
-				<cd:artist>Bob Dylan</cd:artist>
-				<cd:country rdf:resource="http://www.countries.org/onto/USA"/>
-				<cd:company>Columbia</cd:company>
-				<cd:price>10.90</cd:price>
-				<cd:year>1985</cd:year>
-				</rdf:Description>
-	
-				<rdf:Description
-				rdf:about="http://www.recshop.fake/cd/Hide your heart">
-				<cd:artist>Bonnie Tyler</cd:artist>
-				<cd:country>UK</cd:country>
-				<cd:company>CBS Records</cd:company>
-				<cd:price>9.90</cd:price>
-				<cd:year>1988</cd:year>
-				</rdf:Description>
-	
-				<rdf:Description
-				rdf:about="http://www.countries.org/onto/USA">
-				<rdfs:label>USA</rdfs:label>
-				</rdf:Description>
-	
-				<rdf:Description rdf:about="http://www.countries.org/onto/Albums">
-				<rdfs:subClassOf rdf:resource="http://www.countries.org/onto/MediaCollections"/>
-				</rdf:Description>
-				</rdf:RDF>';
-		return $testImportData;
-	}
 		
 }
 
