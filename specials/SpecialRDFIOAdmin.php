ee421a03328942ecab327a0944241c4217f54146
Ali King <ali@koshatnik.com>
2014-07-05 22:51:20 +0100

    ARC2Admin page renamed to RDFIO admin

---
+++ specials/SpecialRDFIOAdmin.php
@@ -0,0 +1,25 @@
+<?php
+# Alert the user that this is not a valid entry point to MediaWiki if they try to access the special pages file directly.
+if ( !defined( 'MEDIAWIKI' ) ) {
+    echo <<<EOT
+To install my extension, put the following line in LocalSettings.php:
+require_once( "\$IP/extensions/RDFIO/specials/SpecialRDFIOAdmin.php" );
+EOT;
+    exit( 1 );
+}
+
+$wgExtensionCredits['specialpage'][] = array(
+	'path' => __FILE__,
+	'name' => 'RDFIOAdmin',
+	'author' => 'Samuel Lampa',
+	'url' => 'http://www.mediawiki.org/wiki/Extension:RDFIO',
+	'descriptionmsg' => 'rdfio-arc2admin-desc',
+	'version' => '0.0.0',
+);
+
+$dir = dirname( __FILE__ ) . '/';
+
+$wgAutoloadClasses['RDFIOAdmin'] = $dir . 'SpecialRDFIOAdmin_body.php'; # Tell MediaWiki to load the extension body.
+$wgExtensionMessagesFiles['RDFIOAdmin'] = $dir . '../RDFIO.i18n.php';
+$wgExtensionAliasFiles['RDFIOAdmin'] = $dir . '../RDFIO.alias.php';
+$wgSpecialPages['RDFIOAdmin'] = 'RDFIOAdmin'; # Let MediaWiki know about your new special page.
---
+++ specials/SpecialRDFIOAdmin_body.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * SpecialRDFIOAdmin is a Special page for setting up the database tables for an ARC2 RDF Store
+ * @author samuel.lampa@gmail.com
+ * @package RDFIO
+ */
+class RDFIOAdmin extends SpecialPage {
+
+	protected $isSysop;
+
+	function __construct() {
+		global $wgUser;
+
+		$userGroups = $wgUser->getGroups();
+		if ( in_array( 'sysop', $userGroups ) ) {
+			$this->isSysop = true;
+		} else {
+			$this->isSysop = false;
+		}
+
+		parent::__construct( 'RDFIOAdmin', 'editinterface' );
+    }
+
+    function execute( $par ) {
+		global $wgRequest, $wgOut, $smwgARC2StoreConfig,
+			$wgServer, $wgScriptPath, $wgUser;
+		
+		$wgOut->setPageTitle("RDFIO Admin");
+		$this->setHeaders();
+		$output = "";
+
+		# Get request data from, e.g.
+		$rdfioAction = $wgRequest->getText( 'rdfio_action' );
+
+		# instantiation
+		$store = ARC2::getStore( $smwgARC2StoreConfig );
+
+		$output .= "\n===RDF Store Setup===\n'''Status:'''\n\n";
+
+		if ( !$store->isSetUp() ) {
+			$output .= "* Store is '''not''' set up\n";
+			if ( $rdfioAction == "setup" ) {
+				if ( !$wgUser->matchEditToken( $wgRequest->getText( 'token' ) ) ) {
+					die( 'Cross-site request forgery detected!' );
+				} else {
+					if ( $this->isSysop ) {
+						$output .= "* Setting up now ...\n";
+						$store->setUp();
+						$output .= "* Done!\n";
+					} else {
+						$errorMessage = "Only sysops can perform this operation!";
+						$wgOut->addHTML( "<pre>Permission Error: " . $errorMessage . "</pre>" );
+					}
+				}
+			}
+		} else {
+			$output .= "* Store is already set up.\n";
+		}
+
+		$wgOut->addWikiText( $output );
+
+		$htmlOutput = '<form method="get" action="' . $wgServer . $wgScriptPath . '/index.php/Special:RDFIOAdmin"
+			name="createEditQuery">
+			<input type="submit" name="rdfio_action" value="setup">' .
+			Html::Hidden( 'token', $wgUser->editToken() ) . '
+			</form>';
+
+		$wgOut->addHTML( $htmlOutput );
+		if( !$this->isSysop ) { 
+			$notSysop = "You do not have permission to set up the ARC2 store";
+			$wgOut = "<pre> " . $notSysop . "</pre>";
+		}
+
+	}
+}
