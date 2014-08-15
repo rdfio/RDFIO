0f3c2229970e6e941e8dc0003dfe190156ea0ced
Ali King <ali@koshatnik.com>
2014-08-15 15:16:34 +0100

    Removed WOM text and variables, refactored parsing result variables

--- classes/RDFIO_SMWPageWriter.php
+++ classes/RDFIO_SMWPageWriter.php
@@ -13,51 +13,14 @@ class RDFIOSMWPageWriter {
 		global $wgOut;
 
 		foreach ( $wikiPages as $wikiTitle => $wikiPage ) {
-			// Get property objects from WOM
-			$womPropertyObjs = array();
-			$womCategoryObjs = array();
+			// Get properties, categories, templates and related data from the page
 			$newWikiContent = "";
 			$mwTitleObj = Title::newFromText( $wikiTitle );
 			
-			// If page exists, get it's data from WOM
+			// If page exists, get its data
 			$titleIsObj = is_object($mwTitleObj);
 			$titleExists = $mwTitleObj->exists();
 			if ( $titleIsObj && $titleExists ) {
-/*				$womWikiPage = WOMProcessor::getPageObject( $mwTitleObj );
-				
-				// Get wiki text
-				$wikiContent = $womWikiPage->getWikiText();
-				
-				// Get properties
-				try{
-					$propertyObjIds = WOMProcessor::getObjIdByXPath( $mwTitleObj, '//property' );
-					// use page object functions
-					foreach ( $propertyObjIds as $propertyObjId ) {
-						$womPropertyObj = $womWikiPage->getObject( $propertyObjId );
-						$womPropertyName = $womPropertyObj->getPropertyName();
-						$womPropertyObjs[$womPropertyName] = $womPropertyObj;
-					}
-				} catch( MWException $e ) {
-					// WOM is sending unspecific exceptions that are not really errors ...
-				    //$wgOut->addHTML( '<pre>Exception when talking to WOM: ' . $e->getMessage() . '</pre>' );
-				}
-				
-				// Get categories
-				try {
-					$categoryObjIds = WOMProcessor::getObjIdByXPath( $mwTitleObj, '//category' );
-					foreach ( $categoryObjIds as $categoryObjId ) {
-						$womCategoryObj = $womWikiPage->getObject( $categoryObjId );
-						$womCategoryName = $womCategoryObj->getName();
-						$womCategoryObjs[$womCategoryName] = $womCategoryObj; 
-					}
-				} catch( MWException $e ) {
-					// WOM is sending unspecific exceptions that are not really errors ...
-					//$wgOut->addHTML( '<pre>Exception when talking to WOM: ' . $e->getMessage() . '</pre>' );
-				}
-*/			
-	/*
-	** Below here is experimental stuff 
-	*/	
 
 			$mwPageObj = WikiPage::factory( $mwTitleObj );			
 			$oldWikiContent = $mwPageObj->getText();
@@ -78,33 +41,32 @@ class RDFIOSMWPageWriter {
 
 					// Find all the templates
 			preg_match_all('/\{\{\s?(.*)\s?\|.*\}\}/', $oldWikiContent, $templateMatches);
-			foreach ( $templateMatches[1] as $templateName ) {
-				$mwTemplates[] = $templateName ;  // this will contain the template's properties later
+			foreach ( $templateMatches[1] as $index => $templateName ) {
+				$mwTemplates[$templateName] = array();  // this will contain the template's properties later
+				$mwTemplates[$templateName]['templateCallText'] = $templateMatches[0][$index];
 			}
 
 			if ( !empty($mwTemplates) ) {
 						// Extract the wikitext from each template
-				foreach ( $mwTemplates as $templatePageName ) {
+				foreach ( $mwTemplates as $templatePageName => $array ) {
 					$mwTemplatePageTitle = Title::newFromText( $templatePageName, $defaultNamespace=NS_TEMPLATE );
 					$mwTemplateObj = WikiPage::factory( $mwTemplatePageTitle );
 					$mwTemplateText = $mwTemplateObj->getText();
-					$templateWikiText[$templatePageName] = $mwTemplateText;
-				}
+					$mwTemplates[$templateName]['wikitext'] = $mwTemplateText;
+				
 						// Get the properties and parameter names used in the templates	
-				foreach ( $templateWikiText as $templateName => $mwTemplateText ) {
 					preg_match_all('/\[\[(.*)::\{\{\{(.*)\}\}\}\]\]/', $mwTemplateText, $templateParameterMatches);
 					foreach( $templateParameterMatches[2] as $index => $templateParameter ) {
-						$templateProperties[$templateName][$templateParameter] = $templateParameterMatches[1][$index];
+						$mwTemplates[$templateName]['parameters'][$templateParameter]['property'] = $templateParameterMatches[1][$index];
 					}
-				}
+				
 
 						// Get the parameter values used in the templates
-				foreach ( $templateMatches[0] as $index => $templateContents ) {
-					preg_match_all('/\{\{\s?.*\s?\|(.*)\|?.*\}\}/', $templateContents, $internalText);
+					preg_match_all('/\{\{\s?.*\s?\|(.*)\|?.*\}\}/', $mwTemplates[$templateName]['templateCallText'], $internalText);
 					$templateParameterValues = explode("|", $internalText[1][0]);
 					foreach ( $templateParameterValues as $paramPair ) {
 						$paramValueArray = explode("=", $paramPair);
-						$templateParamValuePairs[$templateMatches[1][$index]][] = array( $paramValueArray[0] => $paramValueArray[1] );
+						$mwTemplates[$templateName]['parameters'][$paramValueArray[0]]['value'] = $paramValueArray[1];
 					}
 				}
 			}
@@ -124,38 +86,6 @@ class RDFIOSMWPageWriter {
 				
 				$isEquivURI = strpos( $pred, "Equivalent URI" ) !== false;
 				$hasLocalUrl = strpos( $obj, "Special:URIResolver" ) !== false;
-				/*if ( $hasLocalUrl && $isEquivURI ) { 
-					// Don't update Equivalent URI if the URL is a local URL (thus containing
-					// "Special:URIResolver").
-				} else if ( !array_key_exists( $predTitleWikified, $womPropertyObjs ) ) { // If property already exists ...
-					$newWomPropertyObj = new WOMPropertyModel( $pred, $obj, '' ); // FIXME: "Property" should not be included in title
-					$newPropertyAsWikiText = $newWomPropertyObj->getWikiText();
-					$newPropertiesAsWikiText .= $newPropertyAsWikiText . "\n";
-				} else { 
-					$womPropertyObj = $womPropertyObjs[$predTitleWikified];
-					
-					// Store the old wiki text for the fact, in order to replace later
-					$oldPropertyText = $womPropertyObj->getWikiText();
-					
-					if ( $isEquivURI ) {
-					    // FIXME: Should be done for all "URL type" facts, not just
-					    //        Equivalent URI:s
-					    // Since this is a URL, it should not be made into a WikiTitle
-					    $newSMWValue = SMWDataValueFactory::newTypeIdValue( '_uri', $obj );
-					} else {
-					    // Create an updated property
-					    $objTitle = Title::newFromText( $obj );					    	
-					    $newSMWValue = SMWWikiPageValue::makePageFromTitle( $objTitle );
-					}
-					
-					$womPropertyObj->setSMWDataValue( $newSMWValue );
-					$newPropertyText = $womPropertyObj->getWikiText();
-						
-					// Replace the existing property with new value
-					$wikiContent = str_replace( $oldPropertyText, $newPropertyText, $wikiContent );
-				}*/
-				// Non-WOM version of the above
-
 				
 				if ( $hasLocalUrl && $isEquivURI ) { 
 					// Don't update Equivalent URI if the URL is a local URL (thus containing
@@ -182,7 +112,9 @@ class RDFIOSMWPageWriter {
 					$newPropertyText = '[[' . $predTitleWikified . '::' . $newValueText . ']]';
 						
 					// Replace the existing property with new value
-					$newWikiContent = str_replace( $oldPropertyText, $newPropertyText, $newWikiContent );
+					if ( $newPropertyText != $oldPropertyText ) {
+						$newWikiContent = str_replace( $oldPropertyText, $newPropertyText, $newWikiContent );
+					}
 				}
 			}			
 			$newWikiContent .= $newPropertiesAsWikiText;
@@ -193,15 +125,7 @@ class RDFIOSMWPageWriter {
 
 				$categoryTitle = Title::newFromText( $category );
 				$categoryTitleWikified = $categoryTitle->getText();
-			/* Old WOM method - temporariy disabled
-	
-				if ( !array_key_exists( $categoryTitleWikified, $womCategoryObjs ) ) { // If property already exists ...
-					$newWomCategoryObj = new WOMCategoryModel( $categoryTitleWikified );
-					$newCategoryAsWikiText = $newWomCategoryObj->getWikiText();
-					$newCategoriesAsWikiText .= $newCategoryAsWikiText . "\n";
-				}  */
-
-			// New non-WOM version!
+				
 				if ( !array_key_exists( $categoryTitleWikified, $mwCategories ) ) {
 					$newCategoriesAsWikiText .= '[[Category:' . $categoryTitleWikified . "]]\n"; // Is there an inbuilt class method to do this?  Can't find one in Category.
 				}
