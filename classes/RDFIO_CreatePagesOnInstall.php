dbb0f8f77f1c403530b782297d6d0f836729070e
Ali King <ali@koshatnik.com>
2014-08-22 22:59:48 +0100

    Add page data to array for creation

--- classes/RDFIO_CreatePagesOnInstall.php
+++ classes/RDFIO_CreatePagesOnInstall.php
@@ -11,11 +11,33 @@ class RDFIOCreatePagesOnInstall {
 	public function create() {
 
 		$wikiPageData = array(
-							'titletext' => array( 'title' => 'titletext', 'content' => 'page content [[Category:Stuff]]', 'summary' => 'Created by RDFIO', 'namespace' => 'NS_MAIN' )
+							'Category:RDFIO Data Source' => array( 'title' => 'RDFIO Data Source',
+												 'content' => 'URL containing RDF data or a SPARQL endpoint which has been used to import triples into the wiki through the RDFIO extension
+																\n [[Has type::URL]]',
+												 'summary' => 'Created by RDFIO',
+												 'namespace' => 'NS_CATEGORY' ),	
+							'Property:RDFIO Import Type' => array( 'title' => 'RDFIO Import Type',
+												 'content' => 'RDF or SPARQL import
+																\n [[Allows value::RDF]] \n [[Allows value::SPARQL]]',
+												 'summary' => 'Created by RDFIO',
+												 'namespace' => 'NS_PROPERTY' ),	
+							'Property:Has template' => array( 'title' => 'Has template',
+												 'content' => 'Template used for pages in this category on creation
+																/n [[Has type::Page]]',
+												 'summary' => 'Created by RDFIO',
+												 'namespace' => 'NS_PROPERTY' ),	
+							'RDF' => array( 'title' => 'RDF',
+												 'content' => 'Resource Description Framwork (RDF)',
+												 'summary' => 'Created by RDFIO',
+												 'namespace' => 'NS_MAIN' ),	
+							'SPARQL' => array( 'title' => 'SPARQL',
+												 'content' => 'SPARQL endpoint',
+												 'summary' => 'Created by RDFIO',
+												 'namespace' => 'NS_MAIN' )
 						);
 
 		foreach ( $wikiPageData as $pageTitle => $page ) {
-			$pageTitleObj => Title::newFromText ($page['title'], $namespace=$page['namespace'];
+			$pageTitleObj => Title::newFromText ($page['title'], $namespace=$page['namespace']);
 			$pageObj = new Article( $pageTitleObj );
 			$pageObj->doEdit( $page['content'], $page['summary'] );
 		}
