fd6554877d575f66940185da069b68cc8eed2fb8
Samuel Lampa <samuel.lampa@gmail.com>
2016-10-28 17:45:19 +0000

    Add setupStore.php maintenance script to enable cli setup of ARC2 tables

---
+++ maintenance/setupStore.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+* To the extent possible under law,  I, Samuel Lampa, have waived all copyright and
+* related or neighboring rights to Hello World. This work is published from Sweden.
+*
+* @copyright CC0 http://creativecommons.org/publicdomain/zero/1.0/
+* @author Samuel Lampa <samuel.lampa@gmail.com>
+* @ingroup Maintenance
+*/
+
+require_once "../../../maintenance/Maintenance.php";
+
+class SetupArc2Store extends Maintenance {
+	public function __construct() {
+		parent::__construct();
+	}
+
+	public function execute() {
+		global $smwgARC2StoreConfig;
+
+        # Get ARC2 Store
+        $store = ARC2::getStore( $smwgARC2StoreConfig );
+
+        if ( !$store->isSetUp() ) {
+            echo( "ARC2 Store is NOT setup, so setting up now ... ");
+			$store->setUp();
+			if ( $store->isSetUp() ) {
+				echo( "Done!\n");
+			} else {
+				echo( "Setup failed with the following errors reported by the ARC2 library:\n");
+				echo( $store->getErrors());
+				exit(1);
+			}
+        } else {
+            echo( "Store is already set up, so not doing anything.\n");
+        }
+	}
+}
+
+$maintClass = 'SetupArc2Store';
+
+require_once RUN_MAINTENANCE_IF_MAIN;
