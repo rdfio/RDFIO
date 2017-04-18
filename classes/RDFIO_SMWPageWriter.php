f01054292892b5fe5b5a7f52b8e57323cc3482d8
Samuel Lampa <samuel.lampa@gmail.com>
2017-04-18 10:25:32 +0200

    Refactor: extract method addNewFactsToWikiText

--- classes/RDFIO_SMWPageWriter.php
+++ classes/RDFIO_SMWPageWriter.php
@@ -72,114 +72,124 @@ class RDFIOSMWPageWriter {
 				$updatedTplCalls[$tplName] = $array['templateCallText'];
 			}
 
-			// --------------------------------------------------------------------------------
+			// Add Facts
+			$newWikiCont = $this->addNewFactsToWikiText( $wikiPage->getFacts(), $updatedTplCalls, $oldTemplates, $oldProperties, $newWikiCont );
 
-			// Add facts (properties) to the wiki text
-			$newPropsAsText = "\n";
-			foreach ( $wikiPage->getFacts() as $fact ) {
-				$pred = $fact['p'];
-				$obj = $fact['o'];
-
-				$predTitleWikified = $this->getWikifiedTitle( $pred );
-
-				$isEquivURI = strpos( $pred, "Equivalent URI" ) !== false;
-				$hasLocalUrl = strpos( $obj, "Special:URIResolver" ) !== false;
-
-				$tplsWithProp = array();
-				$isInTpl = null;
-
-				// Find whether the property is in any template(s) on the page
-				if ( !empty( $oldTemplates ) ) {
-					foreach ( $oldTemplates as $tplName => $array ) {
-						if ( array_key_exists( 'properties', $oldTemplates[$tplName] ) ) {
-							$isInTpl = array_key_exists( $predTitleWikified, $oldTemplates[$tplName]['properties'] );
-							if ( $isInTpl && !in_array( $tplName, $tplsWithProp ) ) {
-								$tplsWithProp[] = $tplName;
-							}
-						}
-					}
-				}
-				$isInPage = array_key_exists( $predTitleWikified, $oldProperties );
-
-				// Set new value - this will be used in different ways depending on whether property is inside or outside template
-				if ( $isEquivURI ) {
-					// FIXME: Should be done for all "URL type" facts, not just
-					//        Equivalent URI:s
-					// Since this is a URL, it should not be made into a WikiTitle
-					$newSMWVal = SMWDataValueFactory::newTypeIdValue( '_uri', $obj );
-				} else {
-					// Create an updated property
-					$objTitle = Title::newFromText( RDFIOUtils::cleanWikiTitle( $obj ) );
-					$newSMWVal = SMWWikiPageValue::makePageFromTitle( $objTitle );
-				}
-				$newValText = $newSMWVal->getWikiValue();
-
-				// Handle updating differently depending on whether property exists in/outside template
-
-				if ( $hasLocalUrl && $isEquivURI ) {
-					// Don't update Equivalent URI if the URL is a local URL (thus containing
-					// "Special:URIResolver").
-				} else if ( $isInTpl ) {
-					// Code to update/add property to template call(s)
-					foreach ( $tplsWithProp as $idx => $tplName ) {
-						$oldTplCall = $updatedTplCalls[$tplName];  // use temp value as may be updated more than once
-						$param = $oldTemplates[$tplName]['properties'][$predTitleWikified];
-						$oldVal = null;
-						$hasOldVal = array_key_exists( 'value', $oldTemplates[$tplName]['parameters'][$param] );
-						if ( $hasOldVal ) {
-							$oldVal = $oldTemplates[$tplName]['parameters'][$param]['value'];
-						}
-						$newParamValText = $param . '=' . $newValText;
-						$newTplCall = $oldTplCall;
-
-						if ( $hasOldVal ) {
-							// if the parameter already had a value and there's a new value, replace this value in the template call
-							if ( $newValText != $oldVal ) {
-								$oldParamValText = $param . '=' . $oldVal;
-								$newTplCall = str_replace( $oldParamValText, $newParamValText, $oldTplCall );
-							}
-						} else {
-							// if the parameter wasn't previously populated, add it to the parameter list in the template call
-							preg_match( '/(\{\{\s?.*\s?\|?.?)(\}\})/', $oldTplCall, $tplCallMatch );
-							if ( !empty( $tplCallMatch ) ) {
-								$tplCallStart = $tplCallMatch[1];
-								$tplCallEnd = $tplCallMatch[2];
-								$newTplCall = $tplCallStart . '|' . $newParamValText . $tplCallEnd;
-							}
-						}
-
-					}
-					if ( $newTplCall != $oldTplCall ) {
-						//  if the template call has been updated, change it in the page wikitext and update the placeholder variable
-						$newWikiCont = str_replace( $oldTplCall, $newTplCall, $newWikiCont );
-						$updatedTplCalls[$tplName] = $newTplCall;
-					}
-
-				} else if ( $isInPage ) {
-					// if it's a standard property in the page, replace value with new one if different
-
-					$oldPropText = $oldProperties[$predTitleWikified]['wikitext'];
-					// Store the old wiki text for the fact, in order to replace later
-
-					$newPropText = '[[' . $predTitleWikified . '::' . $newValText . ']]';
-
-					// Replace the existing property with new value
-					if ( $newPropText != $oldPropText ) {
-						$newWikiCont = str_replace( $oldPropText, $newPropText, $newWikiCont );
-					}
-				} else if ( !$isInPage ) { // If property isn't in the page (outside of templates) ...
-					$newPropAsText = '[[' . $predTitleWikified . '::' . $obj . ']]';
-					$newPropsAsText .= $newPropAsText . "\n";
-				}
-			}
-			$newWikiCont .= $newPropsAsText;
-
-			// Categories
+			// Add Categories
 			$newWikiCont = $this->addNewCategoriesToWikiText( $newCategories, $oldCategories, $newWikiCont );
 
 			// Write to wiki
 			$this->writeToArticle( $wikiTitle, $newWikiCont, 'Update by RDFIO' );
 		}
+	}
+
+	/**
+	 * Add facts to wiki text
+	 * @param array $facts
+	 * @param array $updatedTplCalls
+	 * @param array $oldTemplates
+	 * @param array $oldProperties
+	 * @param string $wikiText
+	 * @return string $wikiText
+	 */
+	private function addNewFactsToWikiText( $facts, $updatedTplCalls, $oldTemplates, $oldProperties, $wikiText ) {
+		$newPropsAsText = "\n";
+		foreach ( $facts as $fact ) {
+			$pred = $fact['p'];
+			$obj = $fact['o'];
+
+			$predTitleWikified = $this->getWikifiedTitle( $pred );
+
+			// Find whether the property is in any template(s) on the page
+			$tplsWithProp = array();
+			$occursInATpl = false;
+			if ( !empty( $oldTemplates ) ) {
+				foreach ( $oldTemplates as $tplName => $tplInfo ) {
+					if ( array_key_exists( 'properties', $oldTemplates[$tplName] ) ) {
+						$occursInATpl = array_key_exists( $predTitleWikified, $oldTemplates[$tplName]['properties'] );
+						if ( $occursInATpl && !in_array( $tplName, $tplsWithProp ) ) {
+							$tplsWithProp[] = $tplName;
+						}
+					}
+				}
+			}
+
+			$isInPage = array_key_exists( $predTitleWikified, $oldProperties );
+
+			// Set new value - this will be used in different ways depending on whether property is inside or outside template
+			$isEquivURI = strpos( $pred, "Equivalent URI" ) !== false;
+			if ( $isEquivURI ) {
+				// FIXME: Should be done for all "URL type" facts, not just
+				//        Equivalent URI:s
+				// Since this is a URL, it should not be made into a WikiTitle
+				$newSMWVal = SMWDataValueFactory::newTypeIdValue( '_uri', $obj );
+			} else {
+				// Create an updated property
+				$objTitle = Title::newFromText( RDFIOUtils::cleanWikiTitle( $obj ) );
+				$newSMWVal = SMWWikiPageValue::makePageFromTitle( $objTitle );
+			}
+			$newValText = $newSMWVal->getWikiValue();
+
+			// Handle updating differently depending on whether property exists in/outside template
+			$hasLocalUrl = strpos( $obj, "Special:URIResolver" ) !== false;
+			if ( $hasLocalUrl && $isEquivURI ) {
+				// Don't update Equivalent URI if the URL is a local URL (thus containing
+				// "Special:URIResolver").
+			} else if ( $occursInATpl ) {
+				// Code to update/add property to template call(s)
+				foreach ( $tplsWithProp as $idx => $tplName ) {
+					$oldTplCall = $updatedTplCalls[$tplName];  // use temp value as may be updated more than once
+					$param = $oldTemplates[$tplName]['properties'][$predTitleWikified];
+					$oldVal = null;
+					$hasOldVal = array_key_exists( 'value', $oldTemplates[$tplName]['parameters'][$param] );
+					if ( $hasOldVal ) {
+						$oldVal = $oldTemplates[$tplName]['parameters'][$param]['value'];
+					}
+					$newParamValText = $param . '=' . $newValText;
+					$newTplCall = $oldTplCall;
+
+					if ( $hasOldVal ) {
+						// if the parameter already had a value and there's a new value, replace this value in the template call
+						if ( $newValText != $oldVal ) {
+							$oldParamValText = $param . '=' . $oldVal;
+							$newTplCall = str_replace( $oldParamValText, $newParamValText, $oldTplCall );
+						}
+					} else {
+						// if the parameter wasn't previously populated, add it to the parameter list in the template call
+						preg_match( '/(\{\{\s?.*\s?\|?.?)(\}\})/', $oldTplCall, $tplCallMatch );
+						if ( !empty( $tplCallMatch ) ) {
+							$tplCallStart = $tplCallMatch[1];
+							$tplCallEnd = $tplCallMatch[2];
+							$newTplCall = $tplCallStart . '|' . $newParamValText . $tplCallEnd;
+						}
+					}
+
+				}
+				if ( $newTplCall != $oldTplCall ) {
+					//  if the template call has been updated, change it in the page wikitext and update the placeholder variable
+					$newWikiCont = str_replace( $oldTplCall, $newTplCall, $wikiText );
+					$updatedTplCalls[$tplName] = $newTplCall;
+				}
+
+			} else if ( $isInPage ) {
+				// if it's a standard property in the page, replace value with new one if different
+
+				$oldPropText = $oldProperties[$predTitleWikified]['wikitext'];
+				// Store the old wiki text for the fact, in order to replace later
+
+				$newPropText = '[[' . $predTitleWikified . '::' . $newValText . ']]';
+
+				// Replace the existing property with new value
+				if ( $newPropText != $oldPropText ) {
+					$newWikiCont = str_replace( $oldPropText, $newPropText, $newWikiCont );
+				}
+			} else if ( !$isInPage ) { // If property isn't in the page (outside of templates) ...
+				$newPropAsText = '[[' . $predTitleWikified . '::' . $obj . ']]';
+				$newPropsAsText .= $newPropAsText . "\n";
+			}
+		}
+		$wikiText .= $newPropsAsText;
+		return $wikiText;
 	}
 
 	/**
