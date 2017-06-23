1f7d5e3044f3aa3cb22f24d17ff1c37832259701
Samuel Lampa <samuel.lampa@gmail.com>
2017-06-23 14:52:03 +0200

    Add shebang

--- tests/ci/emptydb.sh
+++ tests/ci/emptydb.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 echo 'drop database circle_test;' | mysql -u root;
 echo 'create database circle_test;' | mysql -u root;
 echo 'grant all on circle_test.* on ubuntu@localhost;' | mysql -u root;
