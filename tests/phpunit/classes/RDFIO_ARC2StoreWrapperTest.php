6e8ae93680aec1d71f257154640871938ed8344a
Samuel Lampa <samuel.lampa@gmail.com>
2017-06-26 14:18:29 +0000

    Beef up first store wrapper test

--- tests/phpunit/classes/RDFIO_ARC2StoreWrapperTest.php
+++ tests/phpunit/classes/RDFIO_ARC2StoreWrapperTest.php
@@ -12,11 +12,35 @@ class RDFIOARC2StoreWrapperTest extends RDFIOTestCase {
 	}
 
 	public function getEquivURIsForURITest() {
+		$wrapper = new RDFIOARC2StoreWrapper(new FakeTripleStore() );
 
+		$expectedUri = 'http://www.countries.org/onto/USA';
+		$actualUri = $this->invokeMethod( $wrapper, 'getEquivURIsForURI', array( 'http://localhost:8080/w/index.php/Special:URIResolver/USA' ) );
+
+		$this->assertEquals( $expectedUri, $actualUri );
 	}
 
 }
 
 class FakeTripleStore {
-	// ...
+	public function query( $query ) {
+		if ( $query == 'SELECT ?equivUri WHERE { <http://localhost:8080/w/index.php/Special:URIResolver/USA> <http://www.w3.org/2002/07/owl#sameAs> ?equivUri }' ) {
+			$fakeResult = array(
+				'query_type' => 'select',
+				'result' => array(
+					'variables' => array(
+						'equivUri'
+					),
+					'rows' => array(
+						array(
+							'equivUri' => 'http://www.countries.org/onto/USA',
+							'equivUri type' => 'uri',
+							)
+						)
+					),
+				'query_time' => 0.0081660747528076
+			);
+		}
+		return $fakeResult;
+	}
 }
