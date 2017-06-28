ad66bdbc20454d8449b7c6bab0b9676f09b6dc87
Samuel Lampa <samuel.lampa@gmail.com>
2017-06-28 12:13:20 +0000

    Cleaning up RDFIO Admin Special Page

--- specials/SpecialRDFIOAdmin_body.php
+++ specials/SpecialRDFIOAdmin_body.php
@@ -5,20 +5,11 @@
  * @author samuel.lampa@gmail.com
  * @package RDFIO
  */
-class RDFIOAdmin extends SpecialPage {
+class RDFIOAdmin extends RDFIOSpecialPage {
 
 	protected $isSysop;
 
 	function __construct() {
-		global $wgUser;
-
-		$userGroups = $wgUser->getGroups();
-		if ( in_array( 'sysop', $userGroups ) ) {
-			$this->isSysop = true;
-		} else {
-			$this->isSysop = false;
-		}
-
 		parent::__construct( 'RDFIOAdmin', 'editinterface' );
 	}
 
@@ -27,78 +18,88 @@ class RDFIOAdmin extends SpecialPage {
 	 */
 	function execute( $par ) {
 		unset( $par ); // Needed to suppress warning about unused variable which we include just for consistency.
-		global $wgRequest, $wgOut, $smwgARC2StoreConfig,
-			   $wgServer, $wgScriptPath, $wgUser;
-
-		if ( !$this->userCanExecute( $this->getUser() ) ) {
-			$this->displayRestrictionError();
-			return;
-		};
+		global $smwgARC2StoreConfig, $wgScriptPath, $wgServer;
+		$wUser = $this->getUser();
+		$wRequest = $this->getRequest();
+		$wOut = $this->getOutput();
 
 		$this->setHeaders();
-		$output = "";
 
-		# Get request data from, e.g.
-		$rdfioAction = $wgRequest->getText( 'rdfio_action' );
+		$rdfioAction = $wRequest->getText( 'rdfio_action', '' );
 
-		# instantiation
+		$wOut->addHTML("<h3>RDF Store Setup</h3>" );
+
 		$store = ARC2::getStore( $smwgARC2StoreConfig );
-
-		$output .= "\n===RDF Store Setup===\n'''Status:'''\n\n";
-
-		if ( !$store->isSetUp() ) {
-			$output .= "* Store is '''not''' set up\n";
-			if ( $rdfioAction == "setup" ) {
-				if ( !$wgUser->matchEditToken( $wgRequest->getText( 'token' ) ) ) {
-					die( 'Cross-site request forgery detected!' );
-				} else {
-					if ( $this->isSysop ) {
-						$output .= "* Setting up now ...\n";
-						$store->setUp();
-						if ( $store->isSetUp() ) {
-							$output .= "* Done!\n";
-						} else {
-							$output .= "Setup failed:\n";
-							$store->getErrors();
-						}
-					} else {
-						$errorMessage = "Only sysops can perform this operation!";
-						$wgOut->addHTML( "<pre>Permission Error: " . $errorMessage . "</pre>" );
-					}
-				}
-			}
+		if ( $store->isSetUp() ) {
+			$this->infoMsg( 'Store is already set up.' );
 		} else {
-			$output .= "* Store is already set up.\n";
+			if ( $rdfioAction === 'setup' ) {
+				$this->setUpStore( $store, $wUser, $wRequest );
+			} else {
+				$this->infoMsg( 'Store is <b>not</b> set up' );
+				$setupStoreForm = '
+				<form method="get"
+					action="' . $wgServer . $wgScriptPath . '/index.php/Special:RDFIOAdmin"
+					name="createEditQuery">
+					<input
+						type="submit"
+						name="rdfio_action"
+						value="setup">' .
+					Html::Hidden( 'token', $wUser->getEditToken() ) .
+				'</form>';
+				$wOut->addHTML( $setupStoreForm );
+			}
 		}
 
-		$wgOut->addWikiText( $output );
-
-		$htmlOutput = '<form method="get" action="' . $wgServer . $wgScriptPath . '/index.php/Special:RDFIOAdmin"
-			name="createEditQuery">
-			<input type="submit" name="rdfio_action" value="setup">' .
-			Html::Hidden( 'token', $wgUser->getEditToken() ) . '
-			</form>';
-
-		$wgOut->addHTML( $htmlOutput );
-
-		$wgOut->addWikiText( "\n===Data Sources===\n" );
-		$wgOut->addWikiText( "\n{{#ask: [[Category:RDFIO Data Source]]
+		$wOut->addWikiText( "\n===Data Sources===\n" );
+		$wOut->addWikiText( "\n{{#ask: [[Category:RDFIO Data Source]]
 					|?Equivalent URI
 					|?RDFIO Import Type
 					|format=table
 					|mainlabel=Data Source
 					|limit=10
 					}}\n" );
-		$wgOut->addWikiText( "\n===Pages and Templates===\n" );
-		$wgOut->addWikiText( "To associate a template with a category, add <nowiki>[[Has template::Template:Name]]</nowiki> to the Category page" );
 
-		$wgOut->addWikiText( "{{#ask:  [[:Category:+]]
+		$wOut->addWikiText( "\n===Pages and Templates===\n" );
+		$wOut->addWikiText( "To associate a template with a category, add <nowiki>[[Has template::Template:Name]]</nowiki> to the Category page" );
+		$wOut->addWikiText( "{{#ask:  [[:Category:+]]
 					|?Equivalent URI
 					|?Has template
 					|format=table
 					|mainlabel=Category
 					|limit=10
 					}}" );
+	}
 
+	/**
+	 * Check permissions and set up the ARC2 store if allowed.
+	 * @param $store
+	 * @param $wUser
+	 * @param $wRequest
+	 */
+	private function setUpStore( $store, $wUser, $wRequest ) {
+		if ( !$this->editTokenOk( $wUser, $wRequest ) ) {
+			$this->errorMsg( 'Cross-site request forgery detected!' );
+			return;
+		}
+
+		if ( !in_array( 'sysop', $wUser->getGroups() ) ) {
+			$this->errorMsg( 'Permission Error: Only sysops can perform this operation!' );
+			return;
+		}
+
+		$store->setUp();
+
+		if ( $store->getErrors() ) {
+			$this->errorMsg( 'Error setting up store: ' . implode( "\n", $store->getErrors() ));
+			return;
+		}
+
+		if ( !$store->isSetUp() ) {
+			$this->errorMsg( 'Failed ot set up store, for unknown reason (no errors reported)' );
+			return;
+		}
+
+		$this->successMsg( 'Store successfully set up!' );
 	}
 }
