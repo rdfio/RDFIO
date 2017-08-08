28ee07b48efe436542fac3b043c605741a50f109
Samuel Lampa <samuel.lampa@gmail.com>
2017-08-08 20:28:09 +0000

    Add blank node construct to advanced system test

--- tests/systemtest/run_advanced_system_test_circleci.sh
+++ tests/systemtest/run_advanced_system_test_circleci.sh
@@ -9,10 +9,10 @@ php ../../maintenance/importRdf.php --in data/testdata_advanced.ttl
 php ../../../../maintenance/dumpBackup.php --current > advanced_actual.xml
 
 cat advanced_expected.xml | sed -r 's#(</text>|</title>)#\n\1#' | sed 's#<title>#<title>\n#' | grep -vP '[<>]' > advanced_content_expected.xml
-cat advanced_actual.xml | sed -r 's#(</text>|</title>)#\n\1#' | sed 's#<title>#<title>\n#' | grep -vP '[<>]' > advanced_content_actual.xml
+cat advanced_actual.xml | sed -r 's#(</text>|</title>)#\n\1#' | sed 's#<title>#<title>\n#' | grep -vP '[<>]' | sed -r 's/(Blank[\ _]node[\ _])[a-z1-9]+/\1123abc/g' > advanced_content_actual.xml
 
 if ! diff -q advanced_content_{expected,actual}.xml &>/dev/null; then
-	>&2 echo "ERROR: Files differ in advanced system test!" && echo "For details, check with diff {expected,actual}_content_advanced.xml in Rdfio/tests/systemtest"
+	>&2 echo "ERROR: Files differ in advanced system test!" && echo "For details, check with diff advanced_content_{expected,actual}.xml in Rdfio/tests/systemtest"
 	exit 1;
 else
 	echo "Advanced system test passed!";
--- tests/systemtest/run_system_test_circleci.sh
+++ tests/systemtest/run_system_test_circleci.sh
@@ -6,7 +6,7 @@ mysql -u root circle_test < emptydb.sql
 php ../../maintenance/importRdf.php --in data/testdata.ttl
 php ../../../../maintenance/dumpBackup.php --current | sed -r 's#(</text>|</title>)#\n\1#' | sed 's#<title>#<title>\n#' | grep -vP '[<>]' > simple_content_actual.xml
 if ! diff -q simple_content_{expected,actual}.xml &>/dev/null; then
-	>&2 echo "ERROR: Files differ!" && echo "For details, check with diff simple_content_expected.xml simple_content_actual.xml in Rdfio/tests/systemtest";
+	>&2 echo "ERROR: Files differ!" && echo "For details, check with diff simple_content_{expected,actual}.xml in Rdfio/tests/systemtest";
 	exit 1;
 else
 	echo "System test passed!";
