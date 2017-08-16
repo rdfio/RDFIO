4338de2ffe2d4d3adbbba331f92185b24bf2fccd
Samuel Lampa <samuel.lampa@gmail.com>
2017-08-16 16:48:22 +0200

    Bump version to v3.0.0

--- RDFIO.php
+++ RDFIO.php
@@ -13,7 +13,7 @@ if ( !defined( 'MEDIAWIKI' ) ) {
 // -------------------------------------------------------------
 // Extension meta data
 // -------------------------------------------------------------
-define( 'RDFIO_VERSION', 'v2.4.0' );
+define( 'RDFIO_VERSION', 'v3.0.0' );
 
 $GLOBALS['wgExtensionCredits']['other'][] = array(
 	'path' => __FILE__,
