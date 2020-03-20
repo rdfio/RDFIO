998c425f20087447dd934d1d5a22204c2a187620
Samuel Lampa <samuel.lampa@rilnet.com>
2020-03-20 22:18:39 +0100

    Inactivate outdated system tests

--- tests/ci/run-tests.sh
+++ tests/ci/run-tests.sh
@@ -7,7 +7,10 @@ cd $MWDIR/extensions/Rdfio/tests/phpunit/
 php ../../../../tests/phpunit/phpunit.php --use-normal-tables --configuration=suite.rdfio.xml --coverage-clover=coverage.xml --log-junit $CIRCLE_TEST_REPORTS/phpunit/junit.xml
 mv coverage.xml $HOME/project
 
-cd $MWDIR/extensions/Rdfio/tests/systemtest/
-./run_system_test_circleci.sh
-./run_advanced_system_test_circleci.sh
-./run_roundtrip_test_circleci.sh
+# ---------------------------------------------
+# Inactivated for now. Needs a thorough update:
+# ---------------------------------------------
+#cd $MWDIR/extensions/Rdfio/tests/systemtest/
+#./run_simple_test_circleci.sh
+#./run_advanced_system_test_circleci.sh
+#./run_roundtrip_test_circleci.sh
